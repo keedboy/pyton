c67252f6bec717ea4287b62ef5362fedad051c4d
keedboy <114170132+keedboy@users.noreply.github.com>
2023-05-13 17:27:58 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,43 @@
-import random
-win = 0
-for number in range(1,11):
-    if win < 3:
-        player = int(input("Сделай ход: 1-камень 2-ножницы 3-бумага "))
-        if player == 1:
-            print("Ваш ход - камень")
-        if player == 2:
-            print("Ваш ход - ножницы")
-        if player == 3:
-            print("Ваш ход - бумага")
-        comp = random.randint(1,3)
-        if comp == 1:
-            print("Ход компьютера - камень")
-        if comp == 2:
-            print("Ход компьютера - ножницы")
-        if comp == 3:
-            print("Ход компьютера - бумага")
-        if comp == player:
-            print("Ничья")
-            win = win+0.5
-        if comp == 1 and player == 2 or comp == 2 and player == 3 or comp == 3 and player == 1:
-            print("Компухтер выграл")
-        if comp == 2 and player == 1 or comp == 3 and player == 2 or comp == 1 and player == 3:
-            print("Ты выграл")
-            win = win+1
+secret_word = input("Введите слово:")
+print("\033[2J")
+counter = 1
+points = [0, 0, 0]
+boolean = False
+all_letters = list()
+true_letters = list()
+while boolean == False:
+    if counter == 1:
+        letter = input("Игрок 1 введите букву:")
+    if counter == 2:
+        letter = input("Игрок 2 введите букву:")
+    if counter == 3:
+        letter = input("Игрок 3 введите букву:")
+    if letter in secret_word:
+        print("ты угадал!!!")
+        true_letters.append(letter)
+        if counter == 1:
+            points[0] += 1
+            print("Ваши очки:",points[0])
+        if counter == 2:
+            points[1] += 1
+            print("Ваши очки:",points[1])
+        if counter == 3:
+            points[2] += 1
+            print("Ваши очки:",points[2])
+    if letter in all_letters:
+        print("Буква уже была!!!" )
+    for symbol in secret_word:
+        if symbol in true_letters:
+            print(symbol, end="")
+        else:
+            print("*", end="")
+    all_letters.append(letter)
+    print("\nСписок: ",all_letters, "\n")
+    counter+=1
+    if counter == 4:
+        counter = 1
+    if set(true_letters) == set(secret_word):
+        boolean = True
+print("Игрок 1! Ваши очки: ",points[0])
+print("Игрок 2! Ваши очки: ",points[1])
+print("Игрок 3! Ваши очки: ",points[2])
