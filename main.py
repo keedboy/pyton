4a82fa361c1181d9b5214c434a81f207ab338994
keedboy <114170132+keedboy@users.noreply.github.com>
2023-05-20 16:15:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,20 @@ boolean = False
 all_letters = list()
 true_letters = list()
 while boolean == False:
-    letter = input(f"Игрок {counter}  введите букву:") 
+    letter = input(f"Игрок {counter} введите букву:") 
+    if letter in all_letters:
+        print("Буква уже была!!!" )
+        continue
     if letter in secret_word:
         print("ты угадал!!!")
         true_letters.append(letter)
         points[counter-1] += 1
     print("Ваши очки:",points[counter-1])
-    if letter in all_letters:
-        print("Буква уже была!!!" )
+    if set(true_letters) == set(secret_word):
+        boolean = True
+        print("Игрок 1! Ваши очки: ",points[0])
+        print("Игрок 2! Ваши очки: ",points[1])
+        print("Игрок 3! Ваши очки: ",points[2])
     for symbol in secret_word:
         if symbol in true_letters:
             print(symbol, end="")
@@ -24,8 +30,3 @@ while boolean == False:
     counter+=1
     if counter == 4:
         counter = 1
-    if set(true_letters) == set(secret_word):
-        boolean = True
-print("Игрок 1! Ваши очки: ",points[0])
-print("Игрок 2! Ваши очки: ",points[1])
-print("Игрок 3! Ваши очки: ",points[2])
